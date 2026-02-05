d6e78e2da5386775a06c495598cf8b61a9203b13
vaishnavi26furade <vaishnavi.26furade@gmail.com>
2026-02-05 09:04:37 +0000

    Added Dto for category

---
+++ src/main/java/com/portfolio/portfoliomanager/dto/CategoryDto.java
@@ -0,0 +1,14 @@
+package com.portfolio.portfoliomanager.dto;
+
+import lombok.Data;
+
+@Data
+public class CategoryDto {
+
+    private Long id;
+
+    private String name;
+
+    private String type;
+
+}
